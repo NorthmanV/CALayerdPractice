d4d031cade843ea619987dfa05f01a17678ed814
Ruslan Akberov <ruslanakberov@gmail.com>
2019-08-21 21:05:55 +0300

    Initial Commit

---
+++ CALayersPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CALayersPractice
+//
+//  Created by Ruslan Akberov on 21/08/2019.
+//  Copyright © 2019 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
